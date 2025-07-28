0ce769237e114031137c6fb7d7666be33aa5f682
Saymakh88 <ksayma983@gmail.com>
2025-07-28 20:15:36 +0530

    added Screenshots

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,12 +120,12 @@ ALLOWED_HOSTS = ['sayma123.pythonanywhere.com']
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static"
     ]
 
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
